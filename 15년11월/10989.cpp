50632aa412e8af7ae96df024717ff19edcedc93f
LeeSeongDong <xkdnrl@naver.com>
2015-11-24 11:58:57 +0900

    20151124 - 수 정렬하기3 (10989)

---
+++ 15년11월/10989.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int num[10001];
+
+int main()
+{
+	int n;
+	int m, i, j;
+
+	scanf_s("%d", &n);
+	for (i = 0; i < n; ++i)
+	{
+		scanf_s("%d", &m);
+		++num[m];
+
+	}
+
+	for (i = 1; i < 10001; ++i)
+	{
+		for (j = 0; j < num[i]; ++j)
+		{
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
