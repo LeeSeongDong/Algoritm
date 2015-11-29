d26e324c253a920e0a90b2a39f73b9327636f45b
LeeSeongDong <xkdnrl@naver.com>
2015-11-29 21:58:04 +0900

    20151129 - 최대값

---
+++ 15년11월/2562.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num[9];
+
+	int max = 0;
+	int index;
+	for (int i = 0; i < 9; ++i)
+	{
+		cin >> num[i];
+
+		if (num[i] > max)
+		{
+			max = num[i];
+			index = i;
+		}
+	}
+
+	cout << max << endl << index + 1 << endl;
+
+
+	return 0;
+}
